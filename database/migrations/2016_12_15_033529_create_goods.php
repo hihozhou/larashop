0485f3ab4d0e62911ad85a298f36dcf2404786e3
hihozhou <hihozhou@gmail.com>
2017-01-22 18:16:53 +0800

    购物车表

--- database/migrations/2016_12_15_033529_create_goods.php
+++ database/migrations/2016_12_15_033529_create_goods.php
@@ -18,7 +18,7 @@ class CreateGoods extends Migration
             $table->string('name')->comment('商品名称');
             $table->tinyInteger('is_sale')->default(0)->comment('是否销售：0否，1是');
             $table->string('sku_top_id')->comment('商品sku最高级别id，goods_skus中pid=0');
-            $table->integer('sku_type_ids')->comment('商品sku id list，goods_skus中第二级别');
+            $table->string('sku_type_ids')->comment('商品sku id list，goods_skus中第二级别');
             $table->string('desc')->comment('商品简单描述');
             $table->text('description')->comment('参数说明');
             $table->text('content')->comment('商品图文内容');
---
+++ database/migrations/2017_01_22_095231_create_carts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->comment('用户id');
+            $table->integer('goods_detail_id')->comment('商品详细id');
+            $table->integer('num')->default(1)->comment('数量');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
